4d77c9db73f5024b410e0ccada5c1f06dac232ba
daniloaraujosp <danilo.araujo.sp@gmail.com>
2024-04-08 16:53:24 -0300

    atualização aulas

--- estruturaDadosObjetos/classes.js
+++ estruturaDadosObjetos/classes.js
@@ -3,8 +3,21 @@ class formaDeBolo {
         this.saborDaMassa = saborDaMassa
         this.saborRecheio = saborRecheio
     }
+
+    escrever(){
+        console.log(`Um delicioso bolo de ${this.saborDaMassa} com recheio de ${this.saborRecheio}`)
+    }
+
+    assar(){
+        console.log("Assando bolo de " + this.saborDaMassa)
+    }
 }
 
-let boloFesta = new formaDeBolo("Massa de chocolate", "Recheio de Nutella")
+let boloFesta = new formaDeBolo("chocolate", "Nutella")
+let boloPremium = new formaDeBolo("baunilha", "coco")
 
-console.log(boloFesta.saborRecheio)
+boloFesta.saborDaMassa = "floresta negra"
+
+boloFesta.escrever()
+boloPremium.escrever()
+boloPremium.assar()
